f9cc69c1dfe48c71d3e4e2f9b4a3eba8bfa442ce
papageg <greggordon98@gmail.com>
2018-09-26 21:10:55 -0700

    final project
    Sending in to get graded

--- js/script.js
+++ js/script.js
@@ -1,49 +1,14 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
-// Add variables that store DOM elements you will need to reference and/or manipulate
-const ul = document.querySelector('.student-list');
-const li = ul.children;
-const divStudentDetails = li.children;
-const avatar = document.querySelectorAll('.avatar');
-const h3 = document.querySelectorAll('h3');
-const yearJoined = document.querySelectorAll('.date');
-const arrayOfTenStudents = [li[9], li[19], li[29], li[39], li[49], li[59]];
-/*
-const firstTen = li[9];
-const secondTen = li[19];
-const thirdTen = li[29];
-const fourthTen = li[39];
-const fifthTen = li[49];
-const sixthTen = li[59];
-*/
-console.log(firstTen);
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-/*
-function hideAllItems (students , page, numStudent) {
-  //Create number genertor to grab 10 Students
-  // Need to add numbers to each li student
-  if (let i = numStudent; i <= page; i++) {
-
-  }
-  let firstStudent = 10 * page;
-  let lastStudent = 10 * (page + 1) - 1;
-  if () {
-
-  }
-}
-showStudents(li, 1);
-*/
-
-/*
-function showStudents(list, page) {
+//document.addEventListener("DOMContentLoaded", () => {
+  // Add variables
+  const ul = document.querySelector(".student-list");
+  const liItems = ul.children;
+  const pagediv = document.querySelector(".page");
+  // Create a function to hide all of the items in the list excpet for the ten to show
+  function showStudents(list, page) {
     let pageFirstIndex = 10 * page;
     let pageLastIndex = 10 * (page + 1) - 1;
     for (let i = 0; i < list.length; i++) {
-      let listIndex = Array.prototype.indexOf.call(ul.children, list[i]);
+      let listIndex = Array.prototype.indexOf.call(liItems, list[i]);
 
       if (listIndex >= pageFirstIndex && listIndex <= pageLastIndex) {
         list[i].style.display = "";
@@ -52,12 +17,45 @@ function showStudents(list, page) {
       }
     }
   }
-//showStudents(li, 1);
-*/
-// Create and append the pagination links - Creating a function that can do this is a good approach
 
-
-
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
+  // Create and append the pagination links
+  const appendPages = list => {
+    //creates 5 pages since math.floor on 5.4 is 5
+    const pagesNeeded = Math.floor(list.length / 10);
+    //create a div element to hold pagination
+    const div = document.createElement("div");
+    div.className = "pagination";
+    //append new div to bottom of page
+    pagediv.appendChild(div);
+    //creat and unordered list and appened to the new div
+    const ul = document.createElement("ul");
+    div.appendChild(ul);
+    /* loop in pagination and add in 6 pages since the loops runs for 0 then goes to 5 index
+      each time the loops runs it adds a list item and put a anchor with a empty link
+    */
+    for (let i = 0; i <= pagesNeeded; i++) {
+      const li = document.createElement("li");
+      ul.appendChild(li);
+      const a = document.createElement("A");
+      a.href = "#";
+      a.textContent = i + 1;
+      li.appendChild(a);
+      //
+      if (a.textContent === "1")
+        //make a default setting for the first link to be active
+        a.className = "active";
+      div.addEventListener("click", e => {
+        // Adding functionality for the pagination links
+        // by adding the event listener to the div directly and checking if the pagination's link text is equal to 'i +1'
+        if (e.target.textContent == i + 1) {
+            // displaying the students according to which page is being clicked
+          showStudents(list, i);
+          e.target.className = "active";
+        } else {
+          a.className = "inactive";
+        }
+      });
+    }
+  };
+  appendPages(liItems);
+  showStudents(liItems,0);
